41d264856ad39ef2eb314a82d2c0b742b16b9713
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-02-15 13:47:22 +0200

    correcting errror in apointment manager

--- edc_appointment/managers/appointment_manager.py
+++ edc_appointment/managers/appointment_manager.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class AppointmentManager(models.Manager):
     def get_by_natural_key(self, visit_instance, visit_definition_code, subject_identifier_as_pk):
-        RegisteredSubject = models.get_model('registration', 'RegisteredSubject')
+        RegisteredSubject = models.get_model('edc_registration', 'RegisteredSubject')
         VisitDefinition = models.get_model('edc_visit_schedule', 'VisitDefinition')
         registered_subject = RegisteredSubject.objects.get_by_natural_key(subject_identifier_as_pk)
         visit_definition = VisitDefinition.objects.get_by_natural_key(visit_definition_code)
