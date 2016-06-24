b1bb5712b4e605cbedd7ae16f9a4c0733d63af7e
Coulson <ckgathi@bhp.org.bw>
2016-06-24 14:39:13 +0200

    move test models to example app

--- edc_appointment/tests/test_appointment.py
+++ edc_appointment/tests/test_appointment.py
@@ -1,57 +1,8 @@
-from django.db import models
-from django.utils import timezone
 from django.test import TestCase
-
-from edc_appointment.models import AppointmentMixin, AppointmentModelMixin
 
 # from .base_test_case import BaseTestCase
 from .test_visit_schedule import VisitSchedule
-
-
-class RegisteredSubject(models.Model):
-
-    subject_identifier = models.CharField(
-        verbose_name="Subject Identifier",
-        max_length=50,
-        blank=True,
-        db_index=True,
-        unique=True)
-
-    study_site = models.CharField(
-        max_length=50,
-        null=True,
-        blank=True)
-
-    class Meta:
-        app_label = 'edc_appointment'
-
-
-class AppointmentTestModel(AppointmentModelMixin, models.Model):
-
-    registered_subject = models.ForeignKey(RegisteredSubject)
-
-    report_datetime = models.DateTimeField(default=timezone.now())
-
-    def subject_registration_instance(self):
-        """Returns the subject registration instance.
-
-        Overide this method at the APPOINTMENT_MODEL"""
-        return self.registered_subject
-
-    class Meta:
-        app_label = 'edc_appointment'
-
-
-class TestModel(AppointmentMixin):
-
-    APPOINTMENT_MODEL = AppointmentTestModel
-
-    def save(self, *args, **kwargs):
-        self.prepare_appointments()
-        super(TestModel, self).save(*args, **kwargs)
-
-    class Meta:
-        app_label = 'edc_appointment'
+from example.models import TestModel, AppointmentTestModel
 
 
 class TestAppointment(TestCase):
@@ -62,4 +13,4 @@ class TestAppointment(TestCase):
     def test_appointments_creation(self):
         """Text if appoint trigering method creates appointment."""
         TestModel.objects.create()
-#         self.assertEqual(AppointmentTestModel.objects.all().count(), 3)
+        AppointmentTestModel.objects.all().count()
--- example/models.py
+++ example/models.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 from django_crypto_fields.fields import EncryptedCharField
 from django_crypto_fields.crypt_model_mixin import CryptModelMixin
 from edc_sync.models import SyncHistoricalRecords as AuditTrail
@@ -13,7 +15,27 @@ class Crypt(CryptModelMixin, SyncModelMixin, BaseUuidModel):
         unique_together = (('hash', 'algorithm', 'mode'),)
 
 
-class Appointment(AppointmentModelMixin, BaseUuidModel):
+class RegisteredSubject(BaseUuidModel):
+
+    subject_identifier = models.CharField(
+        verbose_name="Subject Identifier",
+        max_length=50,
+        blank=True,
+        db_index=True,
+        unique=True)
+
+    study_site = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True)
+
+    class Meta:
+        app_label = 'example'
+
+
+class AppointmentTestModel(AppointmentModelMixin, BaseUuidModel):
+
+    registered_subject = models.ForeignKey(RegisteredSubject)
 
     history = AuditTrail()
 
@@ -23,6 +45,12 @@ class Appointment(AppointmentModelMixin, BaseUuidModel):
 
 class TestModel(AppointmentMixin, BaseUuidModel):
     """Trigers creation of appointments."""
+
+    APPOINTMENT_MODEL = AppointmentTestModel
+
+    def save(self, *args, **kwargs):
+        self.prepare_appointments()
+        super(TestModel, self).save(*args, **kwargs)
 
     class Meta:
         app_label = 'example'
