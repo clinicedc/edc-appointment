2513b6b634505f0ba09295981450d7a9dd2561b3
Coulson <ckgathi@bhp.org.bw>
2016-07-07 21:30:01 +0200

    time point status test

--- edc_appointment/model_mixins.py
+++ edc_appointment/model_mixins.py
@@ -59,12 +59,14 @@ class TimePointStatusMixin(models.Model):
         return self.close_datetime
 
     def save(self, *args, **kwargs):
+        update_fields = kwargs.get('update_fields') or []
         self.validate_status()
         if self.time_point_status == CLOSED and not self.close_datetime:
             self.close_datetime = timezone.now()
         else:
             self.close_datetime = None
-        self.time_point_status_open_or_raise()
+        if 'time_point_status' not in update_fields:
+            self.time_point_status_open_or_raise()
         super(TimePointStatusMixin, self).save(*args, **kwargs)
 
     def status_display(self):
@@ -113,26 +115,6 @@ class TimePointStatusMixin(models.Model):
 
     class Meta:
         abstract = True
-        
-
-def appointment_model(self):
-    return django_apps.get_app_config('edc_appointment').appointment_model
-
-
-@receiver(post_save, sender=appointment_model)
-@receiver(post_save, weak=False, dispatch_uid="appointment_post_save")
-def appointment_post_save(sender, instance, raw, created, using, **kwargs):
-    """Update the TimePointStatus in appointment if the field is empty."""
-    if not raw:
-        try:
-            print("It has passed here")
-            if not instance.time_point_status:
-                if instance.appt_status == COMPLETE_APPT:
-                    instance.time_point_status = CLOSED
-                instance.save(update_fields=['time_point_status'])
-        except AttributeError as e:
-            if 'time_point_status' not in str(e):
-                raise AttributeError(str(e))
 
 
 class AppointmentModelMixin(TimePointStatusMixin):
@@ -237,3 +219,23 @@ class AppointmentModelMixin(TimePointStatusMixin):
 
     class Meta:
         abstract = True
+
+
+def appointment_model(self):
+    return django_apps.get_app_config('edc_appointment').appointment_model
+
+
+@receiver(post_save, sender=appointment_model)
+@receiver(post_save, weak=False, dispatch_uid="appointment_post_save")
+def appointment_post_save(sender, instance, raw, created, using, **kwargs):
+    """Update the TimePointStatus in appointment if the field is empty."""
+    if not raw:
+        try:
+            print("It has passed here")
+            if not instance.time_point_status:
+                if instance.appt_status == COMPLETE_APPT:
+                    instance.time_point_status = CLOSED
+                instance.save(update_fields=['time_point_status'])
+        except AttributeError as e:
+            if 'time_point_status' not in str(e):
+                raise AttributeError(str(e))
--- edc_appointment/tests/test_time_point_status.py
+++ edc_appointment/tests/test_time_point_status.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from edc_visit_schedule.site_visit_schedules import site_visit_schedules
@@ -5,7 +6,7 @@ from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from example.models import TestModel, Appointment
 from example.visit_schedule import example_visit_schedule
 
-from edc_constants.constants import CLOSED, COMPLETE_APPT
+from edc_constants.constants import CLOSED, COMPLETE_APPT, OPEN, IN_PROGRESS, NEW_APPT, INCOMPLETE, CANCELLED
 
 
 class TestAppointmentMixin(TestCase):
@@ -17,8 +18,72 @@ class TestAppointmentMixin(TestCase):
         self.appointments = Appointment.objects.all()
 
     def test_appointments_time_point_status_closed(self):
-        """Text if appointment trigering method creates appointment."""
+        """Test if appointment with status closed does not throw an error for time_poit_status closed."""
         appointment = self.appointments[0]
         appointment.appt_status = COMPLETE_APPT
-        appointment.save()
+        appointment.save(update_fields=['appt_status'])
+        self.assertEqual(appointment.time_point_status, OPEN)
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        if appointment.appt_status == COMPLETE_APPT:
+            appointment.time_point_status = CLOSED
+            appointment.save(update_fields=['time_point_status'])
+        self.assertEqual(appointment.time_point_status, CLOSED)
+
+    def test_edit_appointment_time_point_status_closed(self):
+        """Test if an appointment with closed time point status throws an error."""
+        appointment = self.appointments[0]
+        appointment.appt_status = COMPLETE_APPT
+        appointment.save(update_fields=['appt_status'])
+        self.assertEqual(appointment.time_point_status, OPEN)
+        # CLose time point status
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        appointment.time_point_status = CLOSED
+        appointment.save(update_fields=['time_point_status'])
+        # Update appointment
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        appointment.appt_status = IN_PROGRESS
+        with self.assertRaisesMessage(ValidationError, 'Data entry for this time point is closed. See TimePointStatus.'):
+            appointment.save(update_fields=['appt_status'])
+
+    def test_appointment_close_time_point_status(self):
+        """Test if an appointment with a status inprogess throws an error if is set to time point status is closed."""
+        appointment = self.appointments[0]
+        appointment.appt_status = IN_PROGRESS
+        appointment.save(update_fields=['appt_status'])
+        # CLose time point status
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        appointment.time_point_status = CLOSED
+        with self.assertRaisesMessage(ValidationError, 'Cannot close timepoint. Appointment status is IN_PROGRESS.'):
+            appointment.save(update_fields=['time_point_status'])
+
+    def test_appointment_close_time_point_status2(self):
+        """Test if an appointment with a status new throws an error if is set to time point status is closed."""
+        appointment = self.appointments[0]
+        self.assertEqual(appointment.appt_status, NEW_APPT)
+        # CLose time point status
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        appointment.time_point_status = CLOSED
+        with self.assertRaisesMessage(ValidationError, 'Cannot close timepoint. Appointment status is NEW.'):
+            appointment.save(update_fields=['time_point_status'])
+
+    def test_appointment_close_time_point_status3(self):
+        """Test if an appointment with a status Incomplete does not throw an error if time point status is closed."""
+        appointment = self.appointments[0]
+        appointment.appt_status = INCOMPLETE
+        appointment.save(update_fields=['appt_status'])
+        self.assertEqual(appointment.appt_status, INCOMPLETE)
+        # CLose time point status
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        appointment.time_point_status = CLOSED
+        self.assertEqual(appointment.time_point_status, CLOSED)
+
+    def test_appointment_close_time_point_status4(self):
+        """Test if an appointment with a status Cancelled does not throw an error if time point status is closed."""
+        appointment = self.appointments[0]
+        appointment.appt_status = CANCELLED
+        appointment.save(update_fields=['appt_status'])
+        self.assertEqual(appointment.appt_status, CANCELLED)
+        # CLose time point status
+        appointment = Appointment.objects.get(pk=appointment.pk)
+        appointment.time_point_status = CLOSED
         self.assertEqual(appointment.time_point_status, CLOSED)
