7c57e595f918e9d3a345da5fc6b9af83de22a9bd
erikvw <ew2789@gmail.com>
2017-10-30 13:24:35 +0200

    move appt_type and facilities to edc_facility

--- edc_appointment/apps.py
+++ edc_appointment/apps.py
@@ -2,9 +2,10 @@ import sys
 
 from dateutil.relativedelta import MO, TU, WE, TH, FR
 from django.apps import AppConfig as DjangoAppConfig
-from django.apps import apps as django_apps
 from django.conf import settings
 from edc_facility import Facility
+
+from .appointment_config import AppointmentConfig
 
 
 class AppConfig(DjangoAppConfig):
@@ -12,12 +13,11 @@ class AppConfig(DjangoAppConfig):
     _holidays = {}
     name = 'edc_appointment'
     verbose_name = "Edc Appointments"
-    appointment_model = 'edc_appointment.appointment'
-    default_appt_type = 'clinic'
-    visit_reverse_relations = {
-        None: 'subjectvisit',
-        'edc_appointment.appointment': 'subjectvisit',
-    }
+    configurations = [
+        AppointmentConfig(
+            model='edc_appointment.appointment',
+            related_visit_model='edc_appointment.subjectvisit')
+    ]
 
     def ready(self):
         from .signals import (
@@ -26,21 +26,9 @@ class AppConfig(DjangoAppConfig):
             delete_appointments_on_post_delete)
 
         sys.stdout.write(f'Loading {self.verbose_name} ...\n')
-        sys.stdout.write(f' * using {self.appointment_model}.\n')
+        for config in self.configurations:
+            sys.stdout.write(f' * {config.name}.\n')
         sys.stdout.write(f' Done loading {self.verbose_name}.\n')
 
-    def visit_model_reverse_attr(self, key=None):
-        return self.visit_reverse_relations.get(key, 'subjectvisit')
-
-
-if settings.APP_NAME == 'edc_appointment':
-
-    from edc_facility.apps import AppConfig as BaseEdcFacilityAppConfig
-
-    class EdcFacilityAppConfig(BaseEdcFacilityAppConfig):
-        country = 'botswana'
-        facilities = {
-            'clinic': Facility(
-                name='clinic',
-                days=[MO, TU, WE, TH, FR],
-                slots=[100, 100, 100, 100, 100])}
+    def get_configuration(self, name=None):
+        return [c for c in self.configurations if c.name == name][0]
