c6764d350c86e84c887a8a55992f144d1db1b185
erikvw <ew2789@gmail.com>
2017-11-24 08:25:38 +0200

    PEP8

--- edc_appointment/models/appointment.py
+++ edc_appointment/models/appointment.py
@@ -1,8 +1,5 @@
-from django.db import models
-from django.db.models.deletion import PROTECT
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
-from edc_base.utils import get_utcnow
 
 from ..managers import AppointmentManager
 from ..model_mixins import AppointmentModelMixin
