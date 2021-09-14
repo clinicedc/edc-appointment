c7ca664cdae5f211c2ba481461d14504ecf04b8b
erikvw <ew2789@gmail.com>
2021-09-13 20:50:38 -0500

    auths

--- edc_appointment/tests/tests/test_auths.py
+++ edc_appointment/tests/tests/test_auths.py
@@ -2,15 +2,17 @@ from importlib import import_module
 
 from django.test import TestCase, override_settings, tag
 from edc_auth.auth_updater import AuthUpdater
-from edc_auth.site_auths import site_auths
+from edc_data_manager.auth_objects import DATA_MANAGER_ROLE
+from edc_export.auth_objects import EXPORT
 
 
-@tag("1")
 class TestAuths(TestCase):
     @override_settings(
         EDC_AUTH_SKIP_SITE_AUTHS=True,
-        EDC_AUTH_SKIP_AUTH_UPDATER=True,
+        EDC_AUTH_SKIP_AUTH_UPDATER=False,
     )
     def test_load(self):
-        import_module(f"edc_appointment.auths")
+        AuthUpdater.add_empty_groups_for_tests(EXPORT)
+        AuthUpdater.add_empty_roles_for_tests(DATA_MANAGER_ROLE)
+        import_module("edc_appointment.auths")
         AuthUpdater(verbose=True)
