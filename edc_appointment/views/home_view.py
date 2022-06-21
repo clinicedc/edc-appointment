b3cfca9e195ed3b74ce776c2eedbeee94306d985
erikvw <ew2789@gmail.com>
2022-06-21 13:03:04 -0500

    build, get boot strap from func

--- edc_appointment/views/home_view.py
+++ edc_appointment/views/home_view.py
@@ -1,8 +1,8 @@
-from django.conf import settings
 from django.views.generic.base import TemplateView
+from edc_dashboard.utils import get_bootstrap_version
 from edc_dashboard.view_mixins import EdcViewMixin
 
 
 class HomeView(EdcViewMixin, TemplateView):
 
-    template_name = f"edc_appointment/bootstrap{settings.EDC_BOOTSTRAP}/home.html"
+    template_name = f"edc_appointment/bootstrap{get_bootstrap_version()}/home.html"
