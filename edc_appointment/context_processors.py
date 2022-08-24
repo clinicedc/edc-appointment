46f178693dd2f4fb6c7f236315806d2b7f8eb8e5
erikvw <ew2789@gmail.com>
2022-08-24 09:39:53 -0500

    remove constant fropm edc-visit-tracking

--- edc_appointment/context_processors.py
+++ edc_appointment/context_processors.py
@@ -1,5 +1,3 @@
-from edc_visit_tracking.constants import MISSED_VISIT
-
 from edc_appointment.constants import (
     COMPLETE_APPT,
     IN_PROGRESS_APPT,
@@ -15,7 +13,6 @@ def constants(request) -> dict:
         INCOMPLETE_APPT=INCOMPLETE_APPT,
         IN_PROGRESS_APPT=IN_PROGRESS_APPT,
         MISSED_APPT=MISSED_APPT,
-        MISSED_VISIT=MISSED_VISIT,
         NEW_APPT=NEW_APPT,
     )
     return dct
