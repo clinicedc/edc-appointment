21238692b1d1cc8ae8cd88dba9a61bbd1718fe65
erikvw <ew2789@gmail.com>
2023-09-26 19:42:38 -0500

    update_skipped_appointments order CRF by report_datetime

--- edc_appointment/management/commands/update_skipped_appointments.py
+++ edc_appointment/management/commands/update_skipped_appointments.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
                     .objects.filter(
                         subject_identifier=subject_identifier, visit_code_sequence=0
                     )
-                    .order_by("timepoint")
+                    .order_by("subject_visit__report_datetime")
                 )
                 try:
                     crf_obj = crf_model_cls.objects.get(subject_visit=subject_visit)
