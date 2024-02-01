521efe2abcf78ac47103880e234f80ce8aca11e8
erikvw <ew2789@gmail.com>
2024-01-31 19:43:51 -0600

    simplify exception message

--- edc_appointment/form_validator_mixins/window_period_form_validator_mixin.py
+++ edc_appointment/form_validator_mixins/window_period_form_validator_mixin.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 from datetime import datetime
 from typing import TYPE_CHECKING
 
+from dateutil.relativedelta import relativedelta
 from django.conf import settings
-from edc_utils import convert_php_dateformat, to_utc
+from django.utils.translation import gettext as _
+from edc_utils import convert_php_dateformat, formatted_date, to_utc
 from edc_utils.date import ceil_datetime, floor_secs
 from edc_visit_schedule.exceptions import (
     ScheduledVisitWindowError,
@@ -57,22 +59,31 @@ class WindowPeriodFormValidatorMixin:
                     visit_code_sequence=appointment.visit_code_sequence,
                     baseline_timepoint_datetime=self.baseline_timepoint_datetime(appointment),
                 )
-            except UnScheduledVisitWindowError as e:
+            except UnScheduledVisitWindowError:
                 if not self.ignore_window_period_for_unscheduled(
                     appointment, proposed_appt_datetime
                 ):
                     # TODO: fix the dates on this message to match e.message
-                    lower = floor_secs(get_lower_datetime(appointment)).strftime(
-                        datetimestring
-                    )
-                    upper = floor_secs(get_lower_datetime(appointment.next)).strftime(
-                        datetimestring
-                    )
+                    lower = floor_secs(get_lower_datetime(appointment))
+                    try:
+                        # one day less than the next related_visit, if it exists
+                        upper = floor_secs(
+                            appointment.next.related_visit.report_datetime
+                            - relativedelta(days=1)
+                        )
+                    except AttributeError:
+                        # lower bound of next appointment
+                        upper = floor_secs(get_lower_datetime(appointment.next))
+                    dt_lower = formatted_date(lower)
+                    dt_upper = formatted_date(upper)
                     self.raise_validation_error(
                         {
                             form_field: (
-                                "Invalid. Expected a date/time between "
-                                f"{lower} and {upper} (U). Got {e}."
+                                _(
+                                    "Invalid. Expected a date between "
+                                    "%(dt_lower)s and %(dt_upper)s (U)."
+                                )
+                                % dict(dt_lower=dt_lower, dt_upper=dt_upper)
                             )
                         },
                         UNSCHEDULED_WINDOW_ERROR,
